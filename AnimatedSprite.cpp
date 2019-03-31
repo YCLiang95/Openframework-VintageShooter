78571f7324ba7f1897c8e5c9778ad97e5e12142d
ycliang <ycliang95@gmail.com>
2019-03-30 18:36:26 -0700

    Animated Sprite

--- AnimatedSprite.cpp
+++ AnimatedSprite.cpp
@@ -1,1 +1,51 @@
 #include "AnimatedSprite.h"
+#include "Sprite.h"
+
+void AnimatedSprite::load(string path) {
+	image.clear();
+	ofImage* t = new ofImage();
+	t->load(path);
+	image.push_back(t);
+}
+
+void AnimatedSprite::draw(Transform t) {
+	ofPushMatrix();
+	//Transform based on parent position
+	ofTranslate(t.getPosition());
+	//transform based on parent rotation
+	ofRotate(t.getAngle() * 180 / PI);
+	ofPushMatrix();
+	//The center of the sprite will be used by default
+	ofTranslate(-image[position]->getWidth() / 2, -image[position]->getHeight() / 2);
+	image[position]->draw(0, 0);
+	ofPopMatrix();
+	ofPopMatrix();
+}
+
+void AnimatedSprite::splite(int x, int y, int height, int width, int column, int row) {
+	count = column * row;
+	ofImage* im = image[0];
+	image.clear();
+	for (int i = 0; i < column; i++)
+		for (int j = 0; j < row; j++) {
+			ofImage* t = new ofImage();
+			t->cropFrom(*im, x + i * width, y + j * height, width, height);
+			image.push_back(t);
+		}
+};
+
+void AnimatedSprite::update() {
+	if (play) {
+		float dt = ofGetElapsedTimeMillis() - timeLastUpdate;
+		if (dt > interval) {
+			position++;
+			if (position >= count)
+				position = 0;
+			timeLastUpdate = ofGetElapsedTimeMillis();
+		}
+	}
+	else {
+		position = 0;
+		timeLastUpdate = ofGetElapsedTimeMillis();
+	}
+}
--- src/AnimatedSprite.h
+++ src/AnimatedSprite.h
@@ -3,7 +3,19 @@
 
 class AnimatedSprite :public Sprite {
 public:
-	float interval;
+	vector<ofImage*> image;
+	//Load image from path
+	void load(string path);
 
-	void draw();
+	void splite(int x, int y, int height, int width, int column, int row);
+
+	void update();
+	void draw(Transform t);
+	//offset from image files, not in use yet
+	int offset_x, offset_y;
+	int position = 0;
+	float interval = 100;
+	float timeLastUpdate;
+	bool play = false;
+	int count;
 };
